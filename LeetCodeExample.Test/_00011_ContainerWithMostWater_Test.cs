81ec07501e3a936ad4dbf99c758f8b7edb893a44
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:22 -0700

    MaxWaterArea

---
+++ LeetCodeExample.Test/_00011_ContainerWithMostWater_Test.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeExample.Test
+{
+    /// <summary>
+     //   Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate(i, ai). n vertical lines are drawn such that the two endpoints of the line i is at(i, ai) and(i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+     // Notice that you may not slant the container.
+    /// </summary>
+    public class _00011_ContainerWithMostWater_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        public int MaxArea(int[] height)
+        {
+            int lo = 0;
+            int hi = height.Length - 1;
+            int maxArea = 0;
+            while (lo <= hi)
+            {
+                int area = Math.Min(height[lo], height[hi]) * (hi - lo);
+                maxArea = Math.Max(maxArea, area);
+                if (height[lo] < height[hi])
+                    lo++;
+                else
+                    hi--;
+            }
+            return maxArea;
+        }
+    }
+}
