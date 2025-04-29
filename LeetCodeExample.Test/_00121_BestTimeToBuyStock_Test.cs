5a16ac48cba10b54197dfb06a65d794e83e57ec4
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:00 -0700

    stocks

--- LeetCodeExample.Test/_00121_BestTimeToBuyStock_Test.cs
+++ LeetCodeExample.Test/_00121_BestTimeToBuyStock_Test.cs
@@ -25,6 +25,56 @@ namespace LeetCodeExample.Test
 
         public int MaxProfit(int[] prices)
         {
+            int lowestPriceYet = prices[0];
+            // We can only do one single transaction in total and 
+            // buy must happen before the sale.
+            // So just keep track of the lowest price yet seen and take the difference
+            // between that and that days price to find the profit.
+            // Max profit wins
+            int max = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                max = Math.Max(max, prices[i] - lowestPriceYet);
+                lowestPriceYet = Math.Min(lowestPriceYet, prices[i]);
+            }
+            return max;
+        }
+
+        public int MaxProfit3(int[] prices)
+        {
+            int maxProfit = 0;
+            int min = Int32.MaxValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[i] - min);
+                min = Math.Min(min, prices[i]);
+            }
+            return maxProfit;
+        }
+
+        public int MaxProfit2(int[] prices)
+        {
+            int currentMaxProfit = 0;
+
+            int currentHighPrice = prices[prices.Length - 1];
+
+            for (int i = prices.Length - 1; i >= 0; i--)
+            {
+                int price = prices[i];
+                if (price > currentHighPrice)
+                {
+                    currentHighPrice = price;
+                }
+                if (currentHighPrice - price > currentMaxProfit)
+                {
+                    currentMaxProfit = currentHighPrice - price;
+                }
+            }
+            return currentMaxProfit;
+        }
+
+        public int MaxProfit4(int[] prices)
+        {
             int currentMaxProfit = 0;
 
             int currentLowPrice = Int32.MaxValue;
