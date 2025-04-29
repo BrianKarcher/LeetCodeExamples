31ceaf35e4530c22878946a269962b3b3bf9fa8e
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:56 -0700

    Oh dear.

--- LeetCodeExample.Test/DP/_00322_CoinChange.cs
+++ LeetCodeExample.Test/DP/_00322_CoinChange.cs
@@ -13,6 +13,26 @@ namespace LeetCodeExample.Test
     /// </summary>
     public class _00322_CoinChange
     {
+        public int coinChange(int[] coins, int amount)
+        {
+            int max = amount + 1;
+            int[] dp = new int[amount + 1];
+            Array.Fill(dp, max);
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++)
+            {
+                for (int j = 0; j < coins.Length; j++)
+                {
+                    if (coins[j] <= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+
+
         public int CoinChange(int[] coins, int amount)
         {
             if (amount == 0)
