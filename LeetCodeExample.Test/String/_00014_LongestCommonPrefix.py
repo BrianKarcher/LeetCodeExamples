6811ee05a1c0a1ac6e860ff10e8a24ae9fb07b14
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:42 -0700

    14

---
+++ LeetCodeExample.Test/String/_00014_LongestCommonPrefix.py
@@ -0,0 +1,14 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs is None or len(strs) == 0:
+            return ""
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != char:
+                    return strs[0][0:i]
+        return strs[0]
