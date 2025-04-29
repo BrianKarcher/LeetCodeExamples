3b15e91b5b75db05ce57b92e5bee8ca727edef96
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:43 -0700

    56

---
+++ LeetCodeExample.Test/Interval/_00056_MergeIntervals.py
@@ -0,0 +1,23 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals or len(intervals) == 1:
+            return intervals
+
+        START = 0
+        END = 1
+        ans = []
+        intervals.sort()
+        # print(f'{intervals}')
+        current = intervals[0]
+        for i in range(1, len(intervals)):
+            interval = intervals[i]
+            if current[END] < interval[START]:
+                ans.append(current)
+                current = interval
+            else:
+                current[END] = max(current[END], interval[END])
+        ans.append(current)
+        return ans
