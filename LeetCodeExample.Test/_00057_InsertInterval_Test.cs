a6759a9c2ac39290d4b5aaeceb3e139ed4bdb814
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:09 -0700

    Intervals.

--- LeetCodeExample.Test/_00057_InsertInterval_Test.cs
+++ LeetCodeExample.Test/_00057_InsertInterval_Test.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCodeExample.Test
 {
@@ -76,6 +77,56 @@ namespace LeetCodeExample.Test
         public int[] Merge(int start1, int end1, int start2, int end2)
         {
             return new int[2] { Math.Min(start1, start2), Math.Max(end1, end2) };
+        }
+
+        //////////////////////////////////////
+        ///
+
+        public int[][] Insert2(int[][] intervals, int[] newInterval)
+        {
+            if (intervals.Length == 0)
+            {
+                return new int[][] { newInterval };
+            }
+            List<int[]> rtn = new();
+            bool hasPlaced = false;
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                if (!hasPlaced && newInterval[1] < intervals[i][0])
+                {
+                    rtn.Add(newInterval);
+                    rtn.Add(intervals[i]);
+                    hasPlaced = true;
+                }
+                else if (!hasPlaced && IsIntersecting(intervals[i][0], intervals[i][1], newInterval[0], newInterval[1]))
+                {
+                    rtn.Add(Combine(intervals[i][0], intervals[i][1], newInterval[0], newInterval[1]));
+                    hasPlaced = true;
+                }
+                else if (rtn.Count > 0 && IsIntersecting(rtn.Last()[0], rtn.Last()[1], intervals[i][0], intervals[i][1]))
+                {
+                    rtn[rtn.Count - 1] = Combine(rtn.Last()[0], rtn.Last()[1], intervals[i][0], intervals[i][1]);
+                }
+                else
+                {
+                    rtn.Add(intervals[i]);
+                }
+            }
+            if (!hasPlaced)
+            {
+                rtn.Add(newInterval);
+            }
+            return rtn.ToArray();
+        }
+
+        public bool IsIntersecting(int s1, int e1, int s2, int e2)
+        {
+            return s2 <= e1 && e2 >= s1;
+        }
+
+        int[] Combine(int s1, int e1, int s2, int e2)
+        {
+            return new int[] { Math.Min(s1, s2), Math.Max(e1, e2) };
         }
     }
 }
