cbb2afc79c4214c7dd32dc8e74febbbed34f7203
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:13 -0700

    template 2

---
+++ LeetCodeExample.Test/Leetcode Templates/DFSTemplate1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeExample.Test.Leetcode_Templates
+{
+    public class DFSTemplate1
+    {
+        /*
+         * Return true if there is a path from cur to target.
+         */
+        bool DFS(Node cur, Node target, HashSet<Node> visited)
+        {
+            if (cur == target)
+                return true;
+
+            for (int i = 0; i < cur.neighbors.Count; i++)
+            {
+                if (visited.Contains(cur.neighbors[i]))
+                    continue;
+                visited.Add(cur.neighbors[i]);
+
+                if (DFS(cur.neighbors[i], target, visited) == true)
+                    return true;
+            }
+            return false;
+        }
+
+        public class Node
+        {
+            public int val;
+            public List<Node> neighbors;
+        }
+    }
+}
