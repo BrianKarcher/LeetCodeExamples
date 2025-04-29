b35585f2b4d5f3ec680947545ce2648de12a9d14
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:25 -0700

    tle

---
+++ LeetCodeExample.Test/DP/_00552_StudentAttendanceRecordII_Test.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeExample.Test
+{
+    /// <summary>
+    //    An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day.The record only contains the following three characters:
+
+    //'A': Absent.
+    //    'L': Late.
+    //    'P': Present.
+    //    Any student is eligible for an attendance award if they meet both of the following criteria:
+
+
+    //    The student was absent ('A') for strictly fewer than 2 days total.
+    //    The student was never late ('L') for 3 or more consecutive days.
+    //    Given an integer n, return the number of possible attendance records of length n that make a student eligible for an attendance award.The answer may be very large, so return it modulo 109 + 7.
+    /// </summary>
+    public class _00552_StudentAttendanceRecordII_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            var ans = CheckRecord(1);
+            Console.WriteLine(ans);
+
+            ans = CheckRecord(2);
+            Console.WriteLine(ans);
+        }
+
+        public long CheckRecord(int n)
+        {
+            total = 0;
+
+            dp(n, 0, 0);
+            return total;
+        }
+
+        long total;
+
+        public void dp(int n, int absentCount, int lateRecurringCount)
+        {
+            // base case
+            if (n == 0)
+            {
+                total++;
+                return;
+            }
+
+            // Can be Present
+            dp(n - 1, absentCount, 0);
+
+            // Can be absent
+            if (absentCount == 0)
+            {
+                dp(n - 1, 1, 0);
+            }
+            // Can be late
+            if (lateRecurringCount < 3)
+            {
+                dp(n - 1, absentCount, lateRecurringCount + 1);
+            }
+        }
+    }
+}
---
+++ LeetCodeExample.Test/GoogleQuestion/G_00833_FindAndReplaceString_Test.cs
@@ -0,0 +1,75 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeExample.Test
+{
+    /// <summary>
+    //    You are given a 0-indexed string s that you must perform k replacement operations on.The replacement operations are given as three 0-indexed parallel arrays, indices, sources, and targets, all of length k.
+
+
+    //    To complete the ith replacement operation:
+
+
+    //    Check if the substring sources[i] occurs at index indices[i] in the original string s.
+    //If it does not occur, do nothing.
+    //    Otherwise if it does occur, replace that substring with targets[i].
+    //For example, if s = "abcd", indices[i] = 0, sources[i] = "ab", and targets[i] = "eee", then the result of this replacement will be "eeecd".
+
+
+    //    All replacement operations must occur simultaneously, meaning the replacement operations should not affect the indexing of each other. The testcases will be generated such that the replacements will not overlap.
+
+
+    //    For example, a testcase with s = "abc", indices = [0, 1], and sources = ["ab", "bc"] will not be generated because the "ab" and "bc" replacements overlap.
+    //Return the resulting string after performing all replacement operations on s.
+
+
+    //    A substring is a contiguous sequence of characters in a string.
+    /// </summary>
+    public class G_00833_FindAndReplaceString_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        public string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+        {
+            List<(int i, string s, string t)> rep = new List<(int, string, string)>();
+            for (int i = 0; i < indices.Length; i++)
+            {
+                rep.Add((indices[i], sources[i], targets[i]));
+            }
+            var orderedRep = rep.OrderBy(i => i.i).ToList();
+
+            // Build new string
+            string rtn = "";
+            // Index in the original string
+            int index = 0;
+            for (int i = 0; i < orderedRep.Count; i++)
+            {
+                // Validate
+                string cmp = s.Substring(orderedRep[i].i, orderedRep[i].s.Length);
+                if (cmp != orderedRep[i].s)
+                {
+                    continue;
+                }
+                // Add what is before this indice
+                rtn += s.Substring(index, orderedRep[i].i - index);
+                // Add the target
+                rtn += orderedRep[i].t;
+                // Increment the index by the SOURCE length
+                index = orderedRep[i].i + orderedRep[i].s.Length;
+            }
+            // Copy whatever is left over
+            rtn += s.Substring(index);
+            return rtn;
+        }
+    }
+}
