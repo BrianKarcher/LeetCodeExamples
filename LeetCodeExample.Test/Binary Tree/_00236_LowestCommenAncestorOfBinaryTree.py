ea50632ff8b2bbe7038a03f317d06a9f02abc7f4
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:22 -0700

    binary tree lca

---
+++ LeetCodeExample.Test/Binary Tree/_00236_LowestCommenAncestorOfBinaryTree.py
@@ -0,0 +1,51 @@
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+
+    def find(self, root: 'TreeNode', p: 'TreeNode', lst) -> bool:
+        if root == None:
+            return False
+        #print(root.val)
+        if root == p:
+            return True
+        #print(lst)
+        if lst == None:
+            #print('List is None')
+            return False
+        if root.left != None:
+            lst.append(root.left)
+            l = self.find(root.left, p, lst)
+            if l == True:
+                return True
+            # Backtrack
+            lst.pop()
+        if root.right != None:
+            lst.append(root.right)
+            r = self.find(root.right, p, lst)
+            if r == True:
+                return True
+            # Backtrack
+            lst.pop()
+        return False
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pList = [root]
+        self.find(root, p, pList)
+        qList = [root]
+        self.find(root, q, qList)
+        # Traverse both lists until they diverge to find LCA
+        lca = None
+        for i in range(len(pList)):
+            if i >= len(qList) or pList[i].val != qList[i].val:
+                break
+            lca = pList[i]
+        return lca
