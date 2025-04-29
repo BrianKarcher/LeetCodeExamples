52f8e65f669b8441d70274723ee72fc644b53827
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:16 -0700

    striings.

---
+++ LeetCodeExample.Test/_00188_BestTimeToBuyAndSellStockIV_Test.cs
@@ -0,0 +1,40 @@
+//using NUnit.Framework;
+//using System;
+//using System.Collections.Generic;
+
+//namespace LeetCodeExample.Test
+//{
+//    /// <summary>
+//   // You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+//   // Find the maximum profit you can achieve.You may complete at most k transactions.
+//   // Note: You may not engage in multiple transactions simultaneously(i.e., you must sell the stock before you buy again).
+//    /// </summary>
+//    public class _00188_BestTimeToBuyAndSellStockIV_Test
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
+
+//        [Test]
+//        public void Test()
+//        {
+//            var answer = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+//            Assert.AreEqual(0, answer[0]);
+//            Assert.AreEqual(1, answer[1]);
+
+//            answer = TwoSum(new int[] { 3, 2, 4 }, 6);
+//            Assert.AreEqual(1, answer[0]);
+//            Assert.AreEqual(2, answer[1]);
+
+//            answer = TwoSum(new int[] { 3, 3 }, 6);
+//            Assert.AreEqual(0, answer[0]);
+//            Assert.AreEqual(1, answer[1]);
+//        }
+
+//        public int MaxProfit(int k, int[] prices)
+//        {
+
+//        }
+//    }
+//}
