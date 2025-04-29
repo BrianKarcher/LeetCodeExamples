09d0c4a23fd0fa9b46de498796cc03da04e4992b
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:33:13 -0700

    Trie

--- LeetCodeExample.Test/Trie/_00208_ImplementTrie.py
+++ LeetCodeExample.Test/Trie/_00208_ImplementTrie.py
@@ -42,6 +42,41 @@ class Trie:
     def startsWith(self, prefix: str) -> bool:
         node = self.search_prefix(prefix)
         return node is not None
+    
+# Alternate (My attempt)
+from typing import Optional
+from typing import Dict, Any
+class Trie:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        node = self.trie
+        for ch in word:
+            if ch not in node:
+                node[ch] = {}
+            node = node[ch]
+        # $ marks the end of a word
+        # This is simpler than having a separate Node class and a boolean flag
+        node['$'] = True
+
+    # Both search and startsWith will call this
+    def find(self, word: str) -> Optional[Dict[str, Any]]:
+        node = self.trie
+        for ch in word:
+            if ch not in node:
+                return None
+            node = node[ch]
+        return node
+
+    def search(self, word: str) -> bool:
+        node = self.find(word)
+        return node is not None and '$' in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.find(prefix)
+        return node is not None
 
 
 # Your Trie object will be instantiated and called as such:
