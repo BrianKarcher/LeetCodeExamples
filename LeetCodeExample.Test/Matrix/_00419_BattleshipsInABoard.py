6ec0fda8c7399dc952a732ec576de07d445f437e
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:26 -0700

    Battleship better

--- LeetCodeExample.Test/Matrix/_00419_BattleshipsInABoard.py
+++ LeetCodeExample.Test/Matrix/_00419_BattleshipsInABoard.py
@@ -2,6 +2,28 @@
 
 # Battleships can only be placed horizontally or vertically on board. In other words, they can only be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), where k can be of any size. At least one horizontal or vertical cell separates between two battleships (i.e., there are no adjacent battleships).
 
+
+
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        count = 0
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == 'X':
+                    if r == 0 and c == 0:
+                        print('top-left')
+                        count += 1
+                        continue
+                    if r == 0:
+                        count += 1 if board[0][c - 1] == '.' else 0
+                        continue
+                    if c == 0:
+                        count += 1 if board[r - 1][0] == '.' else 0
+                        continue
+                    count += 1 if board[r - 1][c] == '.' and board[r][c - 1] == '.' else 0
+        return count
+    
+
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         count = 0
