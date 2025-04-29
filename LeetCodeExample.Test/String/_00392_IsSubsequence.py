8738e8b904bc43a4e732b8c7c866757b878728b0
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:37 -0700

    392

---
+++ LeetCodeExample.Test/String/_00392_IsSubsequence.py
@@ -0,0 +1,17 @@
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        si = 0
+        for c in t:
+            if s[si] == c:
+                si += 1
+                if si == len(s):
+                    return True
+        return False
