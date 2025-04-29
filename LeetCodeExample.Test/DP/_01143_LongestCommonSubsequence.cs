a0804bd6cdd4d23fc501998866901198465f0ded
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:55 -0700

    LCS update.

--- LeetCodeExample.Test/DP/_01143_LongestCommonSubsequence.cs
+++ LeetCodeExample.Test/DP/_01143_LongestCommonSubsequence.cs
@@ -17,6 +17,29 @@ namespace LeetCodeExample.Test
     {
         public int LongestCommonSubsequence(string text1, string text2)
         {
+            int t1Len = text1.Length;
+            int t2Len = text2.Length;
+            int[,] dp = new int[t1Len + 1, t2Len + 1];
+            for (int i = 1; i <= t1Len; i++)
+            {
+                for (int j = 1; j <= t2Len; j++)
+                {
+                    if (text1[i - 1] == text2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+            return dp[t1Len, t2Len];
+        }
+
+
+        public int LongestCommonSubsequence2(string text1, string text2)
+        {
             memo = new int[text1.Length, text2.Length];
             for (int i = 0; i < text1.Length; i++)
             {
