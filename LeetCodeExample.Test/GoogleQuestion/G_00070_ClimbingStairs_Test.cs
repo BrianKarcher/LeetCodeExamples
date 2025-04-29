bd01b5698054e9e2eccd969793a56cb77ff815d7
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:59 -0700

    stairs

--- LeetCodeExample.Test/GoogleQuestion/G_00070_ClimbingStairs_Test.cs
+++ LeetCodeExample.Test/GoogleQuestion/G_00070_ClimbingStairs_Test.cs
@@ -31,9 +31,27 @@ namespace LeetCodeExample.Test
             Assert.AreEqual(1836311903, answer);
         }
 
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+
+        /// <summary>
+        /// /////////////////////////////////
+        /// </summary>
+
         Dictionary<int, int> map;
 
-        public int ClimbStairs(int n)
+        public int ClimbStairs2(int n)
         {
             map = new Dictionary<int, int>();
             return Recurse(n);
