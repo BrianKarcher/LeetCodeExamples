b3da531c21ccef2cfdbf025a0381cde75780d3b1
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:33:03 -0700

    227

--- LeetCodeExample.Test/Stack/_00227_BasicCalculatorII.py
+++ LeetCodeExample.Test/Stack/_00227_BasicCalculatorII.py
@@ -3,6 +3,98 @@
 # You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
 # Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
 
+# Optimal, O(1) memory:
+ADD = '+'
+SUB = '-'
+MUL = '*'
+DIV = '/'
+OPERATIONS = {ADD, SUB, MUL, DIV}
+MUL_DIV = {MUL, DIV}
+class Solution:
+    def calculate(self, s: str) -> int:
+        calcs = {'+': lambda a, b: a + b,
+                 '-': lambda a, b: a - b,
+                 '*': lambda a, b: a * b,
+                 '/': lambda a, b: a // b}
+
+        operand = ''
+        lastNumber = 0
+        result = 0
+        prevOperation = '+'
+        for i in range(len(s)):
+            c = s[i]
+            if c.isdigit():
+                operand += c
+            if not c.isdigit() and not c == ' ' or i == len(s) - 1:
+                # The operand recording is complete
+                currentNumber = int(operand)
+                # Perform multiply and divide in-flight so at the end all we have in the stack is add and sub
+                # print(f'prevOperation: {prevOperation}')
+                if prevOperation == '*':
+                    lastNumber = lastNumber * currentNumber
+                elif prevOperation == '/':
+                    lastNumber = int(lastNumber / currentNumber)
+                elif prevOperation == '-' or prevOperation == '+':
+                    result += lastNumber
+                    lastNumber = currentNumber if prevOperation == '+' else -currentNumber
+                operand = ''
+                # print(f'setting prevOperation to {c}')
+                prevOperation = c
+        result += lastNumber
+        # All that are left are adds, just add them up
+        return result
+
+
+# A little more optimized
+ADD = '+'
+SUB = '-'
+MUL = '*'
+DIV = '/'
+OPERATIONS = {ADD, SUB, MUL, DIV}
+MUL_DIV = {MUL, DIV}
+class Solution:
+    def calculate(self, s: str) -> int:
+        st = []
+        calcs = {'+': lambda a, b: a + b,
+                 '-': lambda a, b: a - b,
+                 '*': lambda a, b: a * b,
+                 '/': lambda a, b: int(a / b)}
+
+        operand = ''
+        prevOperation = '+'
+        for i in range(len(s)):
+            c = s[i]
+            if c.isdigit():
+                operand += c
+            if not c.isdigit() and not c == ' ' or i == len(s) - 1:
+                # The operand recording is complete
+                currentNumber = int(operand)
+                # Perform multiply and divide in-flight so at the end all we have in the stack is add and sub
+                # print(f'prevOperation: {prevOperation}')
+                if prevOperation in {'*', '/'}:
+                    prevNumber = st.pop()
+                    # print(f'{prevOperation}, {prevNumber}, {currentNumber}')
+                    # print(f'adding {calcs[prevOperation](prevNumber, currentNumber)}')
+                    st.append(calcs[prevOperation](prevNumber, currentNumber))
+                elif prevOperation == '-':
+                    # Convert subtractions to adding a negative number.
+                    # e.g. a - b = a + (-b)
+                    # This will help later when we pull the number from the stack.
+                    # print(f'adding {-currentNumber}')
+                    st.append(-currentNumber)
+                else:
+                    # print(f'adding {currentNumber}')
+                    st.append(currentNumber)
+                operand = ''
+                prevOperation = c
+        # print(st)
+        # All that are left are adds, just add them up
+        return sum(st)
+    
+
+# My original solution:
+
+
 ADD = '+'
 SUB = '-'
 MUL = '*'
