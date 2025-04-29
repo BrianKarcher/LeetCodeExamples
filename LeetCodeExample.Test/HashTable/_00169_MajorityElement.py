fac3a18379f74f72c2a3de3cc84337c77ff8c096
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:29 -0700

    old solution

--- LeetCodeExample.Test/HashTable/_00169_MajorityElement.py
+++ LeetCodeExample.Test/HashTable/_00169_MajorityElement.py
@@ -17,18 +17,11 @@ class Solution:
                     votes -= 1
         return candidate
 
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        candidate = -1
-        votes = 0
+        dict = defaultdict(int)
         for num in nums:
-            if votes == 0:
-                candidate = num
-                votes += 1
-            else:
-                if num == candidate:
-                    votes += 1
-                else:
-                    votes -= 1
-        return candidate
+            dict[num] += 1
+        for key, value in dict.items():
+            if value > len(nums) / 2:
+                return key
