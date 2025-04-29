6a55125f7a7d2162aaa944b4be2ad46ffc23ebbc
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:40 -0700

    dijkstra

--- LeetCodeExample.Test/GoogleQuestion/Acres/_01514_PathWithMaximumProbability.cs
+++ LeetCodeExample.Test/GoogleQuestion/Acres/_01514_PathWithMaximumProbability.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LeetCodeExample.Test
+namespace LeetCodeExample.Test.Google
 {
     /// <summary>
     //    You are given an undirected weighted graph of n nodes(0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
