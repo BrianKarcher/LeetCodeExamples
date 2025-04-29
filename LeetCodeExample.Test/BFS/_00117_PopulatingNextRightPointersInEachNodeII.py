4a726fcbdad4a05e4012eaad3c5782446b2a2853
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:23 -0700

    X

---
+++ LeetCodeExample.Test/BFS/_00117_PopulatingNextRightPointersInEachNodeII.py
@@ -0,0 +1,39 @@
+# Given a binary tree
+
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
+# }
+# Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+# Initially, all next pointers are set to NULL.
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+        queue = deque()
+        queue.append(root)
+        while queue:
+            last = None
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if last is not None:
+                    last.next = node
+                last = node
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+        return root
---
+++ LeetCodeExample.Test/Heap/_00355_DesignTwitter.py
@@ -0,0 +1,29 @@
+# Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+# Implement the Twitter class:
+
+# Twitter() Initializes your twitter object.
+# void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+# List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+# void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+# void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+
+class Twitter:
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.followees = collections.defaultdict(set)
+        self.tweets = collections.defaultdict(deque)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))
+        return [t for _, t in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].discard(followeeId)
