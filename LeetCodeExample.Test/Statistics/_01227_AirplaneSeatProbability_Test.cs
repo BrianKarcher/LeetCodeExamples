9c65352669200ed3091100916a29d602ba52d82b
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:17 -0700

    stupid.

--- LeetCodeExample.Test/Statistics/_01227_AirplaneSeatProbability_Test.cs
+++ LeetCodeExample.Test/Statistics/_01227_AirplaneSeatProbability_Test.cs
@@ -20,17 +20,7 @@ namespace LeetCodeExample.Test
         [Test]
         public void Test()
         {
-            var answer = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
-            Assert.AreEqual(0, answer[0]);
-            Assert.AreEqual(1, answer[1]);
 
-            answer = TwoSum(new int[] { 3, 2, 4 }, 6);
-            Assert.AreEqual(1, answer[0]);
-            Assert.AreEqual(2, answer[1]);
-
-            answer = TwoSum(new int[] { 3, 3 }, 6);
-            Assert.AreEqual(0, answer[0]);
-            Assert.AreEqual(1, answer[1]);
         }
 
         public double NthPersonGetsNthSeat(int n)
