8218186cbde1b2675496ee6ce86eb26dc8185ca7
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:33:13 -0700

    17

--- LeetCodeExample.Test/Combinations/_00017_LetterCombinationsOfAPhoneNumber.py
+++ LeetCodeExample.Test/Combinations/_00017_LetterCombinationsOfAPhoneNumber.py
@@ -1,6 +1,29 @@
 # Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
+# A newer attempt.
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        
+        map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        self.ans = []
+        def rec(idx: int, lst: list[str]):
+            if idx == len(digits):
+                self.ans.append(''.join(lst))
+                return
+            
+            for char in map[digits[idx]]:
+                lst.append(char)
+                rec(idx + 1, lst)
+                lst.pop()
+        
+        rec(0, [])
+        return self.ans
+
+# An older attempt. It is worse than my new attempt due to the immutability of strings.
 from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
