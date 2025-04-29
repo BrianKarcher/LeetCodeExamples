9341fa39d24e486350527a862b5d7bbfb69c1896
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:52 -0700

    19

---
+++ LeetCodeExample.Test/LinkedList/_00019_RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,53 @@
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        size = 0
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        current = head
+        prev = dummyHead
+        prevI = 0
+        while current:
+            size += 1
+            current = current.next
+            if size - prevI > n:
+                prev = prev.next
+        # print(f'{prev.val}')
+        prev.next = prev.next.next
+        return dummyHead.next
+    
+
+#################
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        size = 0
+        current = head
+        while current:
+            size += 1
+            current = current.next
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        remove = size - n
+        current = dummyHead.next
+        prev = dummyHead
+        while current:
+            # print(f'{remove}')
+            if remove == 0:
+                prev.next = current.next
+                return dummyHead.next
+            prev = current
+            current = current.next
+            remove -= 1
