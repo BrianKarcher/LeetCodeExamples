f104c8d82d20536c190cc02c3352f38e3f556753
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:44 -0700

    148

--- LeetCodeExample.Test/Sorting/_00148_SortList.py
+++ LeetCodeExample.Test/Sorting/_00148_SortList.py
@@ -1,5 +1,48 @@
 # Given the head of a linked list, return the list after sorting it in ascending order.
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def merge(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+            dummyHead = ListNode(0)
+            tail = dummyHead
+            while list1 and list2:
+                if list1.val < list2.val:
+                    tail.next = list1
+                    list1 = list1.next
+                else:
+                    tail.next = list2
+                    list2 = list2.next
+                tail = tail.next
+            tail.next = list1 if list1 else list2
+            return dummyHead.next
+        
+        def split(node: Optional[ListNode]) -> Optional[ListNode]:
+            slow = None
+            while node and node.next:
+                slow = node if not slow else slow.next
+                node = node.next.next
+            mid = slow.next
+            slow.next = None
+            return mid
+
+        def recurse(node: Optional[ListNode]) -> Optional[ListNode]:
+            if not node or not node.next:
+                return node
+            mid = split(node)
+            # Split the list to left and right and sort them
+            left = recurse(node)
+            right = recurse(mid)
+            # merge the sorted lists
+            return merge(left, right)
+        
+        return recurse(head)
+
 from typing import List, Optional
 # Definition for singly-linked list.
 class ListNode:
