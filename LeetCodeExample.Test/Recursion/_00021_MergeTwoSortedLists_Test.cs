2ca674313a56ae713426bc669f7502a38ddb3894
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:31:19 -0700

    leetcodesssss

---
+++ LeetCodeExample.Test/Recursion/_00021_MergeTwoSortedLists_Test.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeExample.Test
+{
+    /// <summary>
+    // Merge two sorted linked lists and return it as a sorted list.The list should be made by splicing together the nodes of the first two lists.
+    /// </summary>
+    public class _00021_MergeTwoSortedLists_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            return Recurse(l1, l2);
+        }
+
+        public ListNode Recurse(ListNode l1, ListNode l2)
+        {
+            // Base cases
+            if (l1 == null && l2 == null)
+                return null;
+
+            bool pickFirst = true;
+            if (l1 == null)
+            {
+                pickFirst = false;
+            }
+            else if (l2 == null)
+            {
+                pickFirst = true;
+            }
+            else
+            {
+                if (l1.val < l2.val)
+                {
+                    pickFirst = true;
+                }
+                else
+                {
+                    pickFirst = false;
+                }
+            }
+
+            if (pickFirst == true)
+            {
+                return new ListNode(l1.val, Recurse(l1.next, l2));
+            }
+            else
+            {
+                return new ListNode(l2.val, Recurse(l1, l2.next));
+            }
+        }
+
+        // Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+    }
+}
---
+++ LeetCodeExample.Test/Recursion/_00104_MaximumDepthOfBinaryTree_Test.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeExample.Test
+{
+    /// <summary>
+    // Given the root of a binary tree, return its maximum depth.
+    // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    /// </summary>
+    public class _00104_MaximumDepthOfBinaryTree_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            return Recurse(root, 1);
+        }
+
+        public int Recurse(TreeNode node, int level)
+        {
+            // Base case - a leaf, return the depth
+            if (node.right == null && node.left == null)
+                return level;
+
+            int rightDepth = Int32.MinValue;
+            if (node.right != null)
+            {
+                rightDepth = Recurse(node.right, level + 1);
+            }
+
+            int leftDepth = Int32.MinValue;
+            if (node.left != null)
+            {
+                leftDepth = Recurse(node.left, level + 1);
+            }
+
+            return Math.Max(rightDepth, leftDepth);
+        }
+
+        //Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+    }
+}
