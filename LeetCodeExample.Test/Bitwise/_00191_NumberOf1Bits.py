432614c3b43f73906d7321dfb40b536d4b5d25f4
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:37 -0700

    27

--- LeetCodeExample.Test/Bitwise/_00191_NumberOf1Bits.py
+++ LeetCodeExample.Test/Bitwise/_00191_NumberOf1Bits.py
@@ -7,6 +7,15 @@
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        ans = 0
+        while n != 0:
+            if n & 1 == 1:
+                ans += 1
+            n >>= 1
+        return ans
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
         count = 0
         while n != 0:
             # chop off least significant bit
---
+++ LeetCodeExample.Test/Two Pointer/_00027_RemoveElement.py
@@ -0,0 +1,21 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ans = 0
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            if nums[l] != val:
+                ans += 1
+                l += 1
+            else:
+                nums[l] = nums[r]
+                r -= 1
+        return ans
