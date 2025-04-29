8beb565cc1a8997b90aec87074ef96b78234c3c4
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:33 -0700

    456

---
+++ LeetCodeExample.Test/Stack/_00458_132Pattern.py
@@ -0,0 +1,18 @@
+# Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+# Return true if there is a 132 pattern in nums, otherwise, return false.
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        s3 = float('-inf')
+        # Monotonic increasing stack
+        stack = []
+        # Go from right to left
+        # for i in range(len(nums) - 1, -1, -1):
+        for num in reversed(nums):
+            if num < s3:
+                return True
+            while len(stack) != 0 and num > stack[-1]:
+                s3 = stack.pop()
+            stack.append(num)
+        return False
