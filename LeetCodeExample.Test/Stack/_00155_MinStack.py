cfd23c4e8b04092d1d319c2ebef28725be0e0fa9
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:46 -0700

    155

--- LeetCodeExample.Test/Stack/_00155_MinStack.py
+++ LeetCodeExample.Test/Stack/_00155_MinStack.py
@@ -9,6 +9,37 @@
 # int getMin() retrieves the minimum element in the stack.
 # You must implement a solution with O(1) time complexity for each function.
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        # Always add to main stack
+        self.stack.append(val)
+        if not self.minstack or val < self.minstack[-1][0]:
+            # We store the value, and the count of times we have seen it
+            self.minstack.append([val, 1])
+        elif self.minstack[-1][0] == val:
+            self.minstack[-1][1] += 1
+
+    def pop(self) -> None:
+        if self.minstack[-1][0] == self.stack[-1]:
+            self.minstack[-1][1] -= 1
+        if self.minstack[-1][1] == 0:
+            self.minstack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1][0]
+    
+
+###################################
+
 from collections import deque
 import sys
 class MinStack:
