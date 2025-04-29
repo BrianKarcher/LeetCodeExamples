46a1497609942acaa7a2f648b9b9787d173b9592
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:32 -0700

    415

---
+++ LeetCodeExample.Test/String/_00415_AddString.py
@@ -0,0 +1,26 @@
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        l = max(len(num1), len(num2))
+        carry = 0
+        ans = ""
+        for i in range(-1, -l - 1, -1):
+            i1 = 0
+            if len(num1) >= -i:
+                i1 = int(num1[i])
+            i2 = 0
+            if len(num2) >= -i:
+                i2 = int(num2[i])
+            i3 = i1 + i2 + carry
+            if i3 >= 10:
+                i3 = i3 % 10
+                carry = 1
+            else:
+                carry = 0
+            ans = str(i3) + ans
+        if carry != 0:
+            ans = "1" + ans
+        return ans
