539bff55b4704e86a6d5113868ec299a5ed08ed3
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:49 -0700

    67

--- LeetCodeExample.Test/Bit/_00067_AddBinary.py
+++ LeetCodeExample.Test/Bit/_00067_AddBinary.py
@@ -2,6 +2,31 @@
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        m = len(a) - 1
+        n = len(b) - 1
+        carry = 0
+        ans = ""
+        while m >= 0 or n >= 0 or carry:
+            digit = 0
+            if m >= 0 and a[m] == "1":
+                digit += 1
+            if n >= 0 and b[n] == "1":
+                digit += 1
+            digit += carry
+            if digit >= 2:
+                digit -= 2
+                carry = 1
+            else:
+                carry = 0
+            m -= 1
+            n -= 1
+            ans = ("0" if digit == 0 else "1") + ans
+        return ans
+    
+##############
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
         length = max(len(a), len(b))
         carry = False
         rtn = ""
