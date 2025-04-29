26b03840f5871a2f84107dd646c44e91754f3a83
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:36 -0700

    201

---
+++ LeetCodeExample.Test/Bit/_00201_BitwiseANDOfNumbersRange.py
@@ -0,0 +1,8 @@
+# Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while left < right:
+            # turn off rightmost 1-bit
+            right = right & (right - 1)
+        return right
