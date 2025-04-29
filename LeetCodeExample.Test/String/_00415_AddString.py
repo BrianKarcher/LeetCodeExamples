ef5b05078691980dbe881d4b9a90b8f6a4f33921
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:32:32 -0700

    451 redux

--- LeetCodeExample.Test/String/_00415_AddString.py
+++ LeetCodeExample.Test/String/_00415_AddString.py
@@ -4,6 +4,26 @@
 
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
+        ans = ""
+        while i >= 0 or j >= 0 or carry == 1:
+            add = carry
+            if i >= 0:
+                add += int(num1[i])
+                i -= 1
+            if j >= 0:
+                add += int(num2[j])
+                j -= 1
+            ans = str(add % 10) + ans
+            carry = 1 if add >= 10 else 0
+        return ans
+    
+
+
+class Solution:
+     def addStrings(self, num1: str, num2: str) -> str:
         l = max(len(num1), len(num2))
         carry = 0
         ans = ""
