b9f114559a2477fe2c78dc9c41fe8da0b300ea5e
Brian Karcher <briankarcher90630@gmail.com>
2025-04-28 22:33:05 -0700

    238

--- LeetCodeExample.Test/PrefixSum/_00238_ProductOfArrayExceptSelf.py
+++ LeetCodeExample.Test/PrefixSum/_00238_ProductOfArrayExceptSelf.py
@@ -2,6 +2,23 @@
 # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 # You must write an algorithm that runs in O(n) time and without using the division operation.
 
+# Optimal - O(1) space
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
+        # Left to right
+        ans[0] = 1
+        for i in range(1, len(nums)):
+            ans[i] = nums[i - 1] * ans[i - 1]
+        # print(ans)
+        # Right to left
+        m = 1
+        for i in reversed(range(len(nums))):
+            ans[i] = ans[i] * m
+            m *= nums[i]
+        return ans
+
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
